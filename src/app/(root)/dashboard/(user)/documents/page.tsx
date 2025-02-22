63301ee1bba706bfa5094f3c297380c616328a25
shivam anand <shivam850anand@gmail.com>
2025-02-22 12:04:39 +0530

    done

--- src/app/(root)/dashboard/(user)/documents/page.tsx
+++ src/app/(root)/dashboard/(user)/documents/page.tsx
@@ -1,13 +1,21 @@
+import TemplateGallery from "@/components/_documents/template_gallery";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import React from "react";
 
 const DocPage = () => {
   return (
-    <div>
-      <Link href={"/dashboard/documents/1234567"}>
-        <Button>GO</Button>
-      </Link>
+    <div className="page">
+      {/* Sercahbar */}
+      <Input
+        className="max-w-[340px] rounded-2xl bg-secondary p-4"
+        placeholder="Search Template"
+      />
+      <div className="mt-16">
+
+      <TemplateGallery />
+      </div>
     </div>
   );
 };
