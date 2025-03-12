6c7fb1400a64cef38300d57d23cf326b7f9bba1f
shivam anand <shivam850anand@gmail.com>
2025-03-12 13:57:02 +0530

    encryption corrected

--- src/store/api-endpoints/finance.ts
+++ src/store/api-endpoints/finance.ts
@@ -1,10 +1,11 @@
 import { EncryptData } from "@/utils/encrypt";
 import ApiServices from "../middleware/api-services";
+import { Account } from "@/types/finance.types";
 
 const financeServices = ApiServices.injectEndpoints({
   endpoints: (build) => ({
     createAccount: build.mutation({
-      query: (payload) => {
+      query: (payload: Account) => {
         const encryptedPayload = EncryptData(payload);
         return {
           url: "/finance/create-account",
